96d69dcc51b413e38946a2713c91ee641b76e9e6
MilaSoul <72030303+MilaSoul@users.noreply.github.com>
2021-10-31 15:50:19 +0000

    Class Swan (#3)
    Added new class with attr for swan

---
+++ src/animals/Birds.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animals;
+
+/**
+ *
+ * @author milastolbetskaya
+ */
+public class Birds extends Animal{
+    
+    
+
+    public Birds(double size, double weight, int numberOfEyes, int numberOfLegs, String color) {
+        super(size, weight, numberOfEyes, numberOfLegs, color);
+    }
+    
+    
+    
+    
+}
